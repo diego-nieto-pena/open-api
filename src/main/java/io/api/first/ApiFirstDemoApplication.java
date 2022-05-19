39748261b693e0d97c97b0a93d99d21a38a960f3
Diego Nieto <diego.nieto@auto1.com>
2022-05-19 10:03:05 +0200

    Generating endpoints and schemas

---
+++ src/main/java/io/api/first/ApiFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package io.api.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/api/first/ApiFirstDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.api.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
